442fa254c9825998f12774b651627c86f0f24be5
ac2pic <ac2pic@gmail.com>
2020-01-20 12:09:24 -0600

    Updated prestart import list

--- unified-steps/prestart.js
+++ unified-steps/prestart.js
@@ -6,6 +6,10 @@ import "./js/ac2pic/feature/menu/gui/save/save-misc.js";
 
 import "./js/ac2pic/feature/quick-menu/gui/quick-misc.js";
 
+import "./js/ac2pic/impact/feature/base/action-steps/throwable-prop.js";
+
+import "./js/ac2pic/impact/feature/base/entities/moveable-prop.js";
+
 import "./js/bakafish/feature/bee/combat/model/combat-condition-injection.js";
 
 import "./js/bakafish/feature/bee/combat/new-buffs.js";
@@ -22,15 +26,11 @@ import "./js/bakafish/impact/feature/base/action-steps/set-attrib-hp.js";
 
 import "./js/bakafish/impact/feature/base/action-steps/set-tracker-progress.js";
 
-import "./js/bakafish/impact/feature/base/action-steps/throwable-prop.js";
-
 import "./js/bakafish/impact/feature/base/action-steps/timed-action-buff.js";
 
 import "./js/bakafish/impact/feature/base/action-steps/wait-until-guarded-fix.js";
 
 import "./js/bakafish/impact/feature/base/action-steps/wait-until-target-on-ground.js";
-
-import "./js/bakafish/impact/feature/base/entities/moveable-prop.js";
 
 import "./js/bakafish/impact/feature/base/event-steps/play-sound.js";
 

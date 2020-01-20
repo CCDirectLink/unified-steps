4a4f7b43fe120f1c38252c51789031978767b1a3
ac2pic <ac2pic@gmail.com>
2020-01-20 11:38:50 -0600

    Added generated import list

---
+++ unified-steps/prestart.js
@@ -0,0 +1,37 @@
+import "./js/ac2pic/feature/base/entities/map-touch-trigger.js";
+
+import "./js/ac2pic/feature/bee/base/event-steps.js";
+
+import "./js/ac2pic/feature/menu/gui/save/save-misc.js";
+
+import "./js/ac2pic/feature/quick-menu/gui/quick-misc.js";
+
+import "./js/bakafish/feature/bee/combat/model/combat-condition-injection.js";
+
+import "./js/bakafish/feature/bee/combat/new-buffs.js";
+
+import "./js/bakafish/feature/bee/combat/proxy-hp-access.js";
+
+import "./js/bakafish/feature/bee/player/item-consumption-injection.js";
+
+import "./js/bakafish/impact/feature/base/action-steps/circle-attack.js";
+
+import "./js/bakafish/impact/feature/base/action-steps/reset-guarded-hits.js";
+
+import "./js/bakafish/impact/feature/base/action-steps/set-attrib-hp.js";
+
+import "./js/bakafish/impact/feature/base/action-steps/set-tracker-progress.js";
+
+import "./js/bakafish/impact/feature/base/action-steps/throwable-prop.js";
+
+import "./js/bakafish/impact/feature/base/action-steps/timed-action-buff.js";
+
+import "./js/bakafish/impact/feature/base/action-steps/wait-until-guarded-fix.js";
+
+import "./js/bakafish/impact/feature/base/action-steps/wait-until-target-on-ground.js";
+
+import "./js/bakafish/impact/feature/base/entities/moveable-prop.js";
+
+import "./js/bakafish/impact/feature/base/event-steps/play-sound.js";
+
+import "./js/bakafish/impact/feature/base/event-steps/remove-balls.js";
